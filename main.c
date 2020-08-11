4be1acc999581909a412a780d11299cdbae120c5
khushi-kothari <62203565+khushi-kothari@users.noreply.github.com>
2020-08-11 11:43:10 +0530

    Value by reference example.

--- main.c
+++ main.c
@@ -1,66 +1,21 @@
-//An array of 10 integers and stores 5's table in it
 /*
+ Write a program to change the value of a variable to 10 times its original value.
+ Write a function and pass value by refrence.
+ */
+
 #include <stdio.h>
+void multiplier(int *ptr);
 
 int main() {
-    int arr[10];
-    for(int i=1; i<11; i++) {
-        arr[i] = i * 5;
-    }
-    printf("The table of 5 is\n");
-    for(int i=1; i<11; i++) {
-        printf("%d\n", arr[i]);
-    }
+    int i=4;
+    printf("Value of i is 4\n");
+    
+    multiplier(&i);
+    
     return 0;
 }
-*/
 
-
-
-/*//Write a program containing function which counts the number of positive int in an array.
- 
- #include <stdio.h>
- int positiveCheck(int *arr, int n) {
- 
- int positiveNos=0;
- for (int i=0; i<n; i++) {
- if (arr[i] > 0) {
- positiveNos++;
- }
- }
- return positiveNos;
- }
- 
- int main()
- {
- int k;
- //printf("Enter the value of n:\n");
- //scanf("%d", &n);
- int arr[5] = {-1, -4, 78, 2, 60};
- k = positiveCheck(arr, 5);
- printf("There are %d positive nos\n", k);
- 
- return 0;
- }
-*/
-
-
-
-/*
- //Create an array of size 3 X 10 which contains the multiplication table of 2,7 & 9
- 
- #include <stdio.h>
- 
- int main() {
- int basic[3] = {2, 7, 9};
- int arr[3][10];
- 
- for (int i=0; i<3; i++) {
- for (int j=0; j<10; j++) {
- 
- }
- }
- 
- return 0;
- }
- */
+void multiplier(int *ptr) {
+    printf("The value of i in function is %d\n", *ptr);
+    printf("10 times of i is %d\n", (*ptr)*10);
+}

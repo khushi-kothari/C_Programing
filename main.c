1a90ccfc70dedeeaf3f2ceffb750311a02a8760a
khushi-kothari <62203565+khushi-kothari@users.noreply.github.com>
2020-08-11 11:10:32 +0530

    There are comments on each program about its name.
    I will change names of each programs from next tym to reduce sorting.

--- main.c
+++ main.c
@@ -1,17 +1,66 @@
+//An array of 10 integers and stores 5's table in it
 /*
- Find the area of circle then
- modify the same program to find volume of cylinder
- */
 #include <stdio.h>
 
 int main() {
-    double radius, pi = 3.14, height;
-    printf("Enter radius of the circle\n");
-    scanf("%lf" ,&radius);
-    printf("Area of circle is %lf\n", pi*radius*radius);
-    
-    printf("Enter height of the cylinder\n");
-    scanf("%lf" ,&height);
-    printf("Volume of cylinder is %lf\n", pi*radius*radius*height);
+    int arr[10];
+    for(int i=1; i<11; i++) {
+        arr[i] = i * 5;
+    }
+    printf("The table of 5 is\n");
+    for(int i=1; i<11; i++) {
+        printf("%d\n", arr[i]);
+    }
     return 0;
 }
+*/
+
+
+
+/*//Write a program containing function which counts the number of positive int in an array.
+ 
+ #include <stdio.h>
+ int positiveCheck(int *arr, int n) {
+ 
+ int positiveNos=0;
+ for (int i=0; i<n; i++) {
+ if (arr[i] > 0) {
+ positiveNos++;
+ }
+ }
+ return positiveNos;
+ }
+ 
+ int main()
+ {
+ int k;
+ //printf("Enter the value of n:\n");
+ //scanf("%d", &n);
+ int arr[5] = {-1, -4, 78, 2, 60};
+ k = positiveCheck(arr, 5);
+ printf("There are %d positive nos\n", k);
+ 
+ return 0;
+ }
+*/
+
+
+
+/*
+ //Create an array of size 3 X 10 which contains the multiplication table of 2,7 & 9
+ 
+ #include <stdio.h>
+ 
+ int main() {
+ int basic[3] = {2, 7, 9};
+ int arr[3][10];
+ 
+ for (int i=0; i<3; i++) {
+ for (int j=0; j<10; j++) {
+ 
+ }
+ }
+ 
+ return 0;
+ }
+ */

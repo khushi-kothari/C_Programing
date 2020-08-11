131d53765bf7bd076685854f9b6ffa8c2ee51f84
khushi-kothari <62203565+khushi-kothari@users.noreply.github.com>
2020-08-11 12:57:06 +0530

    Reverse an existing array using a function.

---
+++ Reverse-Array.c
@@ -0,0 +1,23 @@
+//Write a program containing a function which reverses the array passed to it
+#include <stdio.h>
+
+void reverse(int *arr, int n) {
+    int temp;
+    
+    for(int i; i<=(n/2); i++) {
+        temp = arr[i];
+        arr[i] = arr[n-i-1];
+        arr[n-i-1] = temp;
+    }
+}
+
+int main()
+{
+    int arr[7] = {1, 2, 3, 4, 5, 6, 7};
+    
+    for(int i; i<=7; i++) {
+        printf("The value of arr[%d] is %d\n", i, arr[i]);
+    }
+    
+    return 0;
+}

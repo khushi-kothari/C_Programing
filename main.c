973a9d390805916d4eef80068cb094d81b8d8a70
khushi-kothari <62203565+khushi-kothari@users.noreply.github.com>
2020-08-11 10:04:27 +0530

    Area of circle and the volume of cylinder.

--- main.c
+++ main.c
@@ -1,14 +1,17 @@
-//To convert deg Celcius to Fahrenheit
-
+/*
+ Find the area of circle then
+ modify the same program to find volume of cylinder
+ */
 #include <stdio.h>
 
 int main() {
-    double degCelcius;
-    printf("Convert °C to °F\n");
-    printf("Enter the value of degree Celcius\n");
-    scanf("%lf", &degCelcius);
-    printf("The conversion from %.2lf°C  is %.2lf°F\n", degCelcius, degCelcius * 9/5 +32);
+    double radius, pi = 3.14, height;
+    printf("Enter radius of the circle\n");
+    scanf("%lf" ,&radius);
+    printf("Area of circle is %lf\n", pi*radius*radius);
     
-    printf("%d\n", 3.0/9);
+    printf("Enter height of the cylinder\n");
+    scanf("%lf" ,&height);
+    printf("Volume of cylinder is %lf\n", pi*radius*radius*height);
     return 0;
 }

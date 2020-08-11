857c8cc7334a0fa2f18c3365aafaf502f025ea0c
khushi-kothari <62203565+khushi-kothari@users.noreply.github.com>
2020-08-11 11:35:31 +0530

    Find Greatest of 3 nos.
    This is a very complex program using only conditional instructors and no loops. It includes all the cases where 2 numbers and 3 numbers are equal.

--- main.c
+++ main.c
@@ -1,66 +1,77 @@
-//An array of 10 integers and stores 5's table in it
-/*
+// Greatest of 3 nos
+
 #include <stdio.h>
 
 int main() {
-    int arr[10];
-    for(int i=1; i<11; i++) {
-        arr[i] = i * 5;
+    int n1, n2, n3;
+    printf("Enter three numbers\n");
+    scanf("%d %d %d", &n1, &n2, &n3);
+    
+    //left side
+    if (n1 > n2){
+        if(n1 > n3 && n2 == n3){
+            printf("n1 is greatest and n1 = n3");
+        }
+        else if(n1 > n3){
+            printf("n1 is greatest");
+        }
+        else{
+            if(n3 > n1){
+                if( n3> n2){
+                    printf("n3 is greatest");
+                }
+                else {
+                    if (n2 > n3) {
+                        printf("Invalid case");
+                    }
+                }
+            }
+                else{
+                    if (n2 > n1 && n2 > n3){
+                        printf("n2 is greatest and n1 = n3");
+                    }
+                    else {
+                        printf("n1 = n3 and greatest");
+                    }
+                }
+            }
     }
-    printf("The table of 5 is\n");
-    for(int i=1; i<11; i++) {
-        printf("%d\n", arr[i]);
+    
+    //right side
+    else {
+        if(n2 > n1) {
+            if(n2 > n3 && n1 == n3) {
+                printf("n2 is greatest and n1 = n3");
+            }
+            else if (n2 > n3) {
+                printf("n2 is greatest");
+            }
+            else {
+                if (n3 > n2) {
+                    if (n3 > n1){
+                        printf("n3 is greatest");
+                    }
+                    else {
+                        if (n2 > n1 && n2 > n3){
+                            printf("n2 is greatest and n1 = n3");
+                        }
+                        else {
+                            printf("n1 = n3 and greatest");
+                        }
+                    }
+                }
+                    else {
+                        printf("n2 = n3 and greaatest");
+                    }
+            }
+        }
+        else if (n3 > n1 && n3 > n2) {
+            printf("n3 is greatest and n1 = n3");
+        }
+        else {
+            printf("n1 = n2 = n3");
+        }
     }
+    
     return 0;
 }
-*/
-
-
-
-/*//Write a program containing function which counts the number of positive int in an array.
- 
- #include <stdio.h>
- int positiveCheck(int *arr, int n) {
- 
- int positiveNos=0;
- for (int i=0; i<n; i++) {
- if (arr[i] > 0) {
- positiveNos++;
- }
- }
- return positiveNos;
- }
- 
- int main()
- {
- int k;
- //printf("Enter the value of n:\n");
- //scanf("%d", &n);
- int arr[5] = {-1, -4, 78, 2, 60};
- k = positiveCheck(arr, 5);
- printf("There are %d positive nos\n", k);
- 
- return 0;
- }
-*/
-
-
-
-/*
- //Create an array of size 3 X 10 which contains the multiplication table of 2,7 & 9
- 
- #include <stdio.h>
- 
- int main() {
- int basic[3] = {2, 7, 9};
- int arr[3][10];
- 
- for (int i=0; i<3; i++) {
- for (int j=0; j<10; j++) {
- 
- }
- }
- 
- return 0;
- }
- */

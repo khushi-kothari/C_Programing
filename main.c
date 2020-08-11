7799853d626cb5fe065750d37a643afa846872ba
khushi-kothari <62203565+khushi-kothari@users.noreply.github.com>
2020-08-11 11:25:29 +0530

    Coversion-Celsius-to-Fahrenheit

--- main.c
+++ main.c
@@ -1,31 +1,14 @@
-//2D array take input from the user (3*2), 3 rows 2 columns
-//Print it using a function
+//To convert deg Celcius to Fahrenheit
 
 #include <stdio.h>
 
-void printArray(int i, int j) {
-    for (i=0; i<3; i++) {
-        for (j=0; j<2; j++) {
-            printf("i=%d j=%d\n", i,j);
-        }
-        printf("\n");
-    }
-}
-
 int main() {
-    int i=3, j=2;
-    int arr[3][2];
-    int *ptr;
-    ptr = &arr[0][0];
+    double degCelcius;
+    printf("Convert °C to °F\n");
+    printf("Enter the value of degree Celcius\n");
+    scanf("%lf", &degCelcius);
+    printf("The conversion from %.2lf°C  is %.2lf°F\n", degCelcius, degCelcius * 9/5 +32);
     
-    for (i=0; i<3; i++) {
-        for (j=0; j<2; j++) {
-            printf("Enter value of %d%d\n", i+1, j+1);
-            ptr++;
-            scanf("%d\n", &arr[i][j]);
-        }
-    }
-    printArray(3,2);
-    
+    printf("%d\n", 3.0/9);
     return 0;
 }
